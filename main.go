aff0f1589d6a83bf5f57a55f0df195893dcae06f
kondo takeshi <chaspy@users.noreply.github.com>
2023-07-02 16:29:12 +0900

    Bump go-github version to v53

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-github/v52/github"
+	"github.com/google/go-github/v53/github"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/oauth2"
